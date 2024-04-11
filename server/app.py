172e9692973c461354cf24d25c62259b1e6b4672
itsgichia <gichiamuiruri@gmail.com>
2024-04-11 23:27:47 +0300

    Task complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,39 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakery_array = []
+    bakeries = Bakery.query.all()
+    for bakery in bakeries:
+        bakery_array.append(bakery.to_dict())
+    return make_response(bakery_array, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    body = ""
+    status = ""
+    if bakery:
+        body = bakery.to_dict()
+        status = 200
+    else:
+        body = f"Error retrieving bakery by bakery id:{id}"
+        status = 404 
+    return make_response(body, 200)
+    
+    
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    array = []
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    for goods in baked_goods:
+        array.append(goods.to_dict())
+    return make_response(array, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return make_response(most_expensive.to_dict(), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
